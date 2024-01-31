a217304dc46bf6953cf8ccb70f5c904ec11fcba3
Alexey <alexeyj4@yandex.ru>
2024-01-31 22:41:18 +0300

    ююю

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -75,7 +75,7 @@ void blink(){
 	//x+=2;
 	//if(x>123)x=1;
 	//OLED_UpdateOnePage(7);
-	OLED_DrawNum(__HAL_TIM_GET_COUNTER(&htim2), 1, 25, 1);
+	OLED_DrawNum((int16_t)__HAL_TIM_GET_COUNTER(&htim2)/4, 1, 25, 1);
 	OLED_UpdateScreen();
 
 
@@ -263,11 +263,11 @@ static void MX_TIM2_Init(void)
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
-  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC1Polarity = TIM_ICPOLARITY_FALLING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
   sConfig.IC1Filter = 10;
-  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC2Polarity = TIM_ICPOLARITY_FALLING;
   sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
   sConfig.IC2Filter = 10;
